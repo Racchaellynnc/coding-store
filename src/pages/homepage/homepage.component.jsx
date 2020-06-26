9d45c66168ed934b2c8150c22483ba64b4f20e8b
Rachael Coder <rachaelcoder@Rachaels-MacBook-Air.local>
2020-06-25 21:38:48 -0700

    added intro picture

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -7,9 +7,15 @@ import './homepage.styles.scss';
 const HomePage = () => (
   <div className="coderality">
     <div className="photo-box">
+      <div className="intro-message">
+        Where We Turn Code Into Reality
+      </div> 
+      <img src="https://i.postimg.cc/MTTDxMpc/binary-1.png" alt="code" className='code-intro'></img>
+    </div>
+    <div className="photo-box">
       <div className="photo-box1" >
         <div className="moving-text-1">
-          Learn how to make coding <br></br>fun for your whole family!
+          Find Inspiration to make coding fun for you. <br></br>
         </div> 
         <div className="optimization-wrapper">
           <img src="https://i.postimg.cc/dQbmKvVs/computer-1.png" alt="brain-optimization" className='brain-optimization'></img>
@@ -17,7 +23,7 @@ const HomePage = () => (
       </div>
         <div className="photo-box2">
           <div className="moving-text">
-            Turn Code into Reality with us.
+            Learn about the Latest VR and AR tech.
           </div> 
           <div className="damian-wrapper">
             <Damian className='damian' />
